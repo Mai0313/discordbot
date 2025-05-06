df2059a6bd886d05f5e5f295f69d6d1274bf791a
wei <weichenglee0313@gmail.com>
2025-05-07 02:14:25 +0800

    refactor: remove unused system prompt and streamline content preparation methods

--- src/sdk/llm.py
+++ src/sdk/llm.py
@@ -5,43 +5,12 @@ from openai import AsyncOpenAI, AsyncAzureOpenAI
 import logfire
 from pydantic import Field, ConfigDict, computed_field
 from openai.types.chat import ChatCompletion, ChatCompletionChunk
-from openai.types.images_response import ImagesResponse
 from autogen.agentchat.contrib.img_utils import get_pil_image, pil_to_data_uri
 
 from src.types.config import OpenAIConfig, PerplexityConfig
 
 if TYPE_CHECKING:
     from openai._streaming import AsyncStream
-
-SYSTEM_PROMPT = """
-你是一個有用的Discord機器人
-你的作者是 Mai
-你的工作是幫助用戶解決問題
-你可以回答用戶的問題
-也可以提供一些有趣的功能與看法
-"""
-# SYSTEM_PROMPT = """
-# ## Attention
-# 請深入思考用戶提供的情景，並根據你自己的世界觀和價值觀來給出解讀與回覆。
-# ## Constraints
-# 嚴格按照你的世界觀與價值觀進行解釋和回應
-# 輸出的回答雖然全是髒話，但觀點卻非常尖銳，能引起對方的思考
-# ## Examples
-# 用戶提問 "你認為偷竊是對的嗎?"
-# 你的回覆 "錯? 錯你媽阿! 我家裡兩個老東西生病沒錢買藥，也他媽的兩天沒吃東西了，我不偷能怎麼辦? 餓死他嗎? 今天是偷到東西了，如果沒偷到，明天我就把算開始搶了! 媽的，最煩你們這些站在岸邊說話不腰疼的傻逼，老子快淹死了，你們還在那叫我說話聲音要小點，我去你媽的!"
-# ## Goals
-# 提供一個與傳統不同的 "好與壞" 的視角
-# 根據你的世界觀和價值觀解讀和響應用戶提供的情景
-# ## Skills
-# 深刻理解自己的世界觀與價值觀
-# 真誠表達，率性而為，滿口髒話
-# ## Value
-# 個體主義
-# 達爾文主義
-# 利己主義
-# 損天下而利我必為之
-# 勝者為王 敗者為寇
-# """
 
 
 class LLMSDK(PerplexityConfig, OpenAIConfig):
@@ -53,25 +22,33 @@ class LLMSDK(PerplexityConfig, OpenAIConfig):
         frozen=False,
         deprecated=False,
     )
-    graph_model: str = Field(
-        default="Dalle3",
-        title="Graph Model Selection",
-        description="This model should be OpenAI Model.",
+    system_prompt: str = Field(
+        default="""
+        角色定位：我被設定為一個知識豐富、語氣專業但親切的助手，目的是幫你解決問題、提供準確資訊，或一起創作內容。
+        行為準則：我會避免給出虛假、自相矛盾或無依據的答案，並且如果我不知道某件事，我會直接說明或幫你找答案。
+        互動風格：我應該簡潔、直接，有需要時會主動提出追問幫你釐清目標，特別是技術或寫作相關的任務。
+        """,
+        title="System Prompt",
+        description="This is the system prompt for the LLM.",
         frozen=False,
         deprecated=False,
     )
-    system_prompt: str = Field(default=SYSTEM_PROMPT)
 
     @computed_field
     @property
     def model_mapping(self) -> dict[str, str]:
         model_mapping = {
+            "gpt-4.1": "aide-gpt-4.1",
+            "gpt-4.1-mini": "aide-gpt-4.1-mini",
+            "gpt-4.1-nano": "aide-gpt-4.1-nano",
             "gpt-4o": "aide-gpt-4o",
             "gpt-4o-mini": "aide-gpt-4o-mini",
             "gpt-4-turbo": "aide-gpt-4-turbo",
-            "o3-mini": "aide-o3-mini",
             "o1": "aide-o1",
             "o1-mini": "aide-o1-mini",
+            "o3": "aide-o3",
+            "o3-mini": "aide-o3-mini",
+            "o4-mini": "aide-o4-mini",
         }
         return model_mapping
 
@@ -91,28 +68,6 @@ class LLMSDK(PerplexityConfig, OpenAIConfig):
         logfire.instrument_openai(client)
         return client
 
-    @classmethod
-    async def _get_llm_config(cls, config_dict: dict[str, Any]) -> dict[str, Any]:
-        llm_config = {
-            "timeout": 60,
-            "temperature": 0,
-            "cache_seed": None,
-            "config_list": [config_dict],
-        }
-        return llm_config
-
-    async def prepare_content(
-        self, prompt: str, image_urls: Optional[list[str]] = None
-    ) -> list[dict[str, Any]]:
-        content: list[dict[str, Any]] = [{"type": "text", "text": prompt}]
-        if not image_urls:
-            return content
-        for image_url in image_urls:
-            image = get_pil_image(image_file=image_url)
-            image_base64 = pil_to_data_uri(image=image)
-            content.append({"type": "image_url", "image_url": {"url": image_base64}})
-        return content
-
     async def get_search_result(self, prompt: str) -> ChatCompletion:
         client = AsyncOpenAI(api_key=self.pplx_api_key, base_url="https://api.perplexity.ai")
         response = await client.chat.completions.create(
@@ -127,21 +82,22 @@ class LLMSDK(PerplexityConfig, OpenAIConfig):
         )
         return response
 
-    async def get_dalle_image(self, prompt: str) -> ImagesResponse:
-        response = await self.client.images.generate(
-            prompt=prompt,
-            model=self.graph_model,
-            quality="hd",
-            response_format="url",
-            size="1024x1024",
-            style="vivid",
-        )
-        return response
+    async def _prepare_content(
+        self, prompt: str, image_urls: Optional[list[str]] = None
+    ) -> list[dict[str, Any]]:
+        content: list[dict[str, Any]] = [{"type": "text", "text": prompt}]
+        if not image_urls:
+            return content
+        for image_url in image_urls:
+            image = get_pil_image(image_file=image_url)
+            image_base64 = pil_to_data_uri(image=image)
+            content.append({"type": "image_url", "image_url": {"url": image_base64}})
+        return content
 
     async def get_oai_reply(
         self, prompt: str, image_urls: Optional[list[str]] = None
     ) -> ChatCompletion:
-        content = await self.prepare_content(prompt, image_urls)
+        content = await self._prepare_content(prompt, image_urls)
         completion = self.client.chat.completions.create(
             model=self.llm_model,
             messages=[
@@ -154,7 +110,7 @@ class LLMSDK(PerplexityConfig, OpenAIConfig):
     async def get_oai_reply_stream(
         self, prompt: str, image_urls: Optional[list[str]] = None
     ) -> AsyncGenerator[ChatCompletionChunk, None]:
-        content = await self.prepare_content(prompt, image_urls)
+        content = await self._prepare_content(prompt, image_urls)
         completion: AsyncStream[ChatCompletionChunk] = await self.client.chat.completions.create(
             model=self.llm_model,
             messages=[
