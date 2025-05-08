1d6e46e4ea33ed29774e10c65c70510165239102
wei <weichenglee0313@gmail.com>
2025-05-09 01:46:08 +0800

    refactor: enhance video download functionality with improved filename handling and pattern matching

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -1,20 +1,31 @@
+import re
 from pathlib import Path
+import datetime
 
 from yt_dlp import YoutubeDL
 from pydantic import Field, BaseModel
+
+pattern = re.compile(r"/([^/]+)/?$")
 
 
 class VideoDownloader(BaseModel):
     output_folder: str = Field(default="downloads", description="Download folder")
 
-    def download(self, url: str) -> str:
+    def download(self, url: str) -> tuple[str, str]:
+        filename = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        match = pattern.search(url)
+        if match:
+            filename_stem = match.group(1)
+            if isinstance(filename_stem, str):
+                filename = filename_stem
+
         output_path = Path(self.output_folder)
         if not output_path.exists():
             output_path.mkdir(parents=True, exist_ok=True)
-
+        outtmpl = f"{output_path.as_posix()}/{filename}.%(ext)s"
         ydl_opts = {
             "format": "best",
-            "outtmpl": str(output_path / "%(title).40s-%(id)s.%(ext)s"),
+            "outtmpl": outtmpl,
             "continuedl": True,
             "restrictfilenames": True,
             "writeinfojson": True,
@@ -23,11 +34,12 @@ class VideoDownloader(BaseModel):
         with YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
             title = info.get("title", "unknown_title")
-            # ydl.download([url])
-        return title
+        return title, outtmpl
 
 
 if __name__ == "__main__":
     downloader = VideoDownloader()
-    url = "https://x.com/reissuerecords/status/1917171960255058421"  # 替换为实际的URL
+    url = "https://x.com/reissuerecords/status/1917171960255058421"
+    url = "https://www.facebook.com/share/r/17h4SsC2p1/"
+    url = "https://www.instagram.com/reels/DFUuxmMPz4n/"
     downloader.download(url)
