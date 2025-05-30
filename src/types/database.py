e2475b7326de2c00911dee23631497242304b11e
wei <weichenglee0313@gmail.com>
2025-05-30 18:49:42 +0800

    fix: update SQLite file path in configuration files

--- src/types/database.py
+++ src/types/database.py
@@ -21,7 +21,7 @@ class PostgreSQLConfig(BaseSettings):
 
 class SQLiteConfig(BaseSettings):
     sqlite_file_path: str = Field(
-        default="sqlite:///data/sqlite.db",
+        default="sqlite:///data/messages.db",
         validation_alias=AliasChoices("SQLITE_FILE_PATH"),
         title="SQLite File Path",
         description="The file path to the SQLite database file.",
