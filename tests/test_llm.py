f850529b0a6a4682401a35cc16d9a1c37db8cdaf
wei <weichenglee0313@gmail.com>
2025-06-09 23:49:58 +0800

    refactor: remove unimplemented test for DALL-E image retrieval

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -17,13 +17,6 @@ async def test_get_oai_reply(llm_sdk: LLMSDK) -> None:
     assert isinstance(response, ChatCompletion)
 
 
-@pytest.mark.skip(reason="This function is not implemented yet.")
-@pytest.mark.asyncio
-async def test_get_dalle_image(llm_sdk: LLMSDK) -> None:
-    response = await llm_sdk.get_dalle_image(prompt=prompt)
-    assert isinstance(response, ImagesResponse)
-
-
 @pytest.mark.asyncio
 async def test_get_oai_reply_stream(llm_sdk: LLMSDK) -> None:
     async for response in llm_sdk.get_oai_reply_stream(prompt=prompt):
