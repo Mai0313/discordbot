65c3f45f4cb820306c567b4d4044c304f3e31007
wei <mai@mai0313.com>
2025-09-18 22:04:05 +0800

    refactor: improve database migration process with data sanitization and enhanced logging
    - Introduced a static method to sanitize DataFrames by removing null characters.
    - Updated the migration process to log table names being migrated.
    - Enhanced the PostgreSQL data insertion with improved handling and chunking.

--- scripts/migrate.py
+++ scripts/migrate.py
@@ -10,44 +10,35 @@ from discordbot.typings.database import DatabaseConfig
 class DatabaseMigration(BaseModel):
     database: DatabaseConfig = Field(default_factory=DatabaseConfig)
 
-    def migrate(self, path_or_data: pd.DataFrame | str) -> None:
-        data = pd.read_csv(path_or_data) if isinstance(path_or_data, str) else path_or_data
-        data["attachments"] = data["attachments"].apply(
-            lambda x: x.replace("data/", "data/attachments/")
-            if isinstance(x, str) and not x.startswith("data/attachments/")
-            else x
-        )
-        engine = create_engine(f"sqlite:///{self.database.sqlite.sqlite_file_path}")
-        groups = data.groupby("channel_name")
-        for name, group in groups:
-            if not name.startswith("DM"):
-                group.to_sql(name=f"channel_{name}", con=engine, if_exists="replace", index=False)
-
-    def split(self, name: str) -> None:
-        engine = create_engine(f"sqlite:///{self.database.sqlite.sqlite_file_path}")
-        data = pd.read_sql_table(table_name=name, con=engine)
-        self.migrate(path_or_data=data)
-        # remove the original table
-        with engine.connect() as conn:
-            conn.execute(f"DROP TABLE {name}")
+    @staticmethod
+    def sanitize_dataframe(data: pd.DataFrame) -> pd.DataFrame:
+        for col in data.select_dtypes(include=["object"]).columns:
+            data[col] = data[col].astype(str).str.replace("\x00", "", regex=False)
+        return data
 
     def migrate_to_postgresql(self, path: str) -> None:
         sqlite_conn = sqlite3.connect(path)
-        pg_engine = create_engine(self.database.postgres.postgres_url)
+        psg_engine = create_engine(self.database.postgres.postgres_url)
         cursor = sqlite_conn.cursor()
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
         tables: list[str] = [row[0] for row in cursor.fetchall()]
 
         for table_name in tables:
-            *_, channel_id = table_name.split("_")
+            print(f"Migrating table: {table_name}")  # noqa: T201
             data = pd.read_sql_query(f"SELECT * FROM `{table_name}`", sqlite_conn)  # noqa: S608
-            data.to_sql(f"channel_{channel_id}", pg_engine, if_exists="append", index=False)
+            data = self.sanitize_dataframe(data)
+            data.to_sql(
+                name=table_name,
+                con=psg_engine,
+                if_exists="append",
+                index=False,
+                chunksize=10_000,
+                method="multi",
+            )
         sqlite_conn.commit()
         sqlite_conn.close()
 
 
 if __name__ == "__main__":
     db_migration = DatabaseMigration()
-    # db_migration.migrate(path_or_data="../data/messages.csv")
-    db_migration.migrate_to_postgresql(path="../data/messages.db")
-    # db_migration.split(name="messages")
+    db_migration.migrate_to_postgresql(path="./data/messages.db")
