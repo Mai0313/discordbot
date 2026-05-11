eba281d1afd4090238840ffff06c5aa61a9637c3
weichenglee <weichenglee@google.com>
2026-05-11 20:23:35 +0800

    fix(tests): remove invalid TikTok URL from download test cases

--- tests/test_download.py
+++ tests/test_download.py
@@ -11,7 +11,6 @@ from discordbot.utils.downloader import VideoDownloader
         "https://x.com/reissuerecords/status/1917171960255058421",
         "https://www.facebook.com/share/r/17h4SsC2p1",
         # "https://www.instagram.com/reels/DFUuxmMPz4n",
-        "https://www.tiktok.com/@zachking/video/6768504823336815877",
     ],
 )
 def test_download(url: str) -> None:
